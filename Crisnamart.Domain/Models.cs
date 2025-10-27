83f64de3b3cfbfcd839d6b47faa557273844d72b
ecthelion5109 <xanderseling@gmail.com>
2025-10-27 19:34:59 -0300

    va quedando muy bien

--- Crisnamart.Domain/Models.cs
+++ Crisnamart.Domain/Models.cs
@@ -1,29 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Crisnamart.Domain;
 
-namespace Crisnamart.Domain;
-public class PromocionModel {
+public record PromocionModel {
+	public required string Titulo { get; init; }
+	public required	int Descuento { get; init; }
+	public required string ImagenUrl { get; init; }
+	public required string Link { get; init; }
+	public required string TextoBoton { get; init; }
+}
 
-	public string Titulo { get; set; } = string.Empty;
-	public int Descuento { get; set; }
-	public string ImagenUrl { get; set; } = string.Empty;
-	public string Link { get; set; } = "#!";
-	public string TextoBoton { get; set; } = "Shop Now";
+public record SlidePromocionalModel {
+	public required string ImagenUrl { get; init; }
+	public required string Subtitulo { get; init; }
+	public required string TituloHtml { get; init; }
+	public required string Descripcion { get; init; }
+	public required string TextoBoton { get; init; }
+	public required string Link { get; init; }
 }
-public class SlidePromocionalModel {
-	public string ImagenUrl { get; set; } = string.Empty;
-	public string Subtitulo { get; set; } = string.Empty;
-	public string TituloHtml { get; set; } = string.Empty; // allows <br/> or <span> tags
-	public string Descripcion { get; set; } = string.Empty;
-	public string TextoBoton { get; set; } = "Shop Now";
-	public string Link { get; set; } = "#!";
-	public string ClasesInternas { get; set; } = "lg:py-32 p-12 lg:pl-12 xl:w-2/5 md:w-3/5";
+
+public record PropuestaValor {
+	public required string Icono { get; init; }
+	public required string Titulo { get; init; }
+	public required string Descripcion { get; init; }
 }
-public class CategoriaDestacadaModel {
-	public string Nombre { get; set; } = string.Empty;
-	public string ImagenUrl { get; set; } = string.Empty;
-	public string Link { get; set; } = "#!";
+
+public record CategoriaModel {
+	public required string Id { get; init; }
+	public required string Nombre { get; init; }
+	public required string ImagenUrl { get; init; }
+	public required string Link { get; init; }
 }
---
+++ Crisnamart.Domain/PrototipoContenido.cs
@@ -0,0 +1,78 @@
+﻿namespace Crisnamart.Domain;
+
+public static class PrototipoContenido {
+
+	public static List<PromocionModel> ObtenerPromociones() => [
+		new PromocionModel {
+			Titulo = "Merca y Falopa",
+			Descuento = 30,
+			ImagenUrl = "images/banner/grocery-banner.png",
+			Link = "#!",
+			TextoBoton = "Encargar ahora!"
+		},
+		new PromocionModel {
+			Titulo = "Faso",
+			Descuento = 25,
+			ImagenUrl = "images/banner/grocery-banner-2.jpg",
+			Link = "#!",
+			TextoBoton = "Hace tu pedido ya!"
+		}
+	];
+
+	public static List<SlidePromocionalModel> ObtenerSlidesPromocionales() => [
+		new SlidePromocionalModel {
+			ImagenUrl = "images/slider/slide-1.jpg",
+			Subtitulo = "Opening Sale Discount 50%",
+			TituloHtml = "SuperMarket<br/>For Fresh Grocery",
+			Descripcion = "Introduced a new model for online grocery shopping and convenient home delivery.",
+			TextoBoton = "Shop Now",
+			Link = "#!"
+		},
+		new SlidePromocionalModel {
+			ImagenUrl = "images/slider/slider-2.jpg",
+			Subtitulo = "Free Shipping - orders over $100",
+			TituloHtml = "Free Shipping on<br/>orders over <span class='text-green-600'>$100</span>",
+			Descripcion = "Free Shipping to First-Time Customers Only, after promotions and discounts are applied.",
+			TextoBoton = "Shop Now",
+			Link = "#!"
+		}
+	];
+
+	public static List<CategoriaModel> TodasLasCategorias => [
+		new CategoriaModel { Id = "1001", Nombre = "Artículos de Cocina", ImagenUrl = "images/category/category-dairy-bread-eggs.jpg", Link = "#!" },
+		new CategoriaModel { Id = "1002", Nombre = "Vajilla y Cubiertos", ImagenUrl = "images/category/category-snack-munchies.jpg", Link = "#!" },
+		new CategoriaModel { Id = "1003", Nombre = "Pavas, Cafeteras y Teteras", ImagenUrl = "images/category/category-bakery-biscuits.jpg", Link = "#!" },
+		new CategoriaModel { Id = "1004", Nombre = "Hogar y Organización", ImagenUrl = "images/category/category-instant-food.jpg", Link = "#!" },
+		new CategoriaModel { Id = "1005", Nombre = "Decoración y Ambientación", ImagenUrl = "images/category/category-tea-coffee-drinks.jpg", Link = "#!" },
+		new CategoriaModel { Id = "1006", Nombre = "Jardín y Camping", ImagenUrl = "images/category/category-atta-rice-dal.jpg", Link = "#!" },
+		new CategoriaModel { Id = "1007", Nombre = "Mascotas y Cuidado", ImagenUrl = "images/category/category-baby-care.jpg", Link = "#!" },
+		new CategoriaModel { Id = "1008", Nombre = "Carpinteria", ImagenUrl = "images/category/category-chicken-meat-fish.jpg", Link = "#!" },
+		new CategoriaModel { Id = "1009", Nombre = "Limpieza", ImagenUrl = "images/category/category-cleaning-essentials.jpg", Link = "#!" },
+		new CategoriaModel { Id = "1000", Nombre = "Animalitos", ImagenUrl = "images/category/category-pet-care.jpg", Link = "#!" }
+	];
+
+	public static List<CategoriaModel> CategoriasDestacadas =>
+		[.. TodasLasCategorias
+		.Where(x => new[] { "1001", "1002", "1003", "1004", "1005", "1006"}
+		.Contains(x.Id))];
+
+	public static List<PropuestaValor> PropuestasDeValor => [
+		new PropuestaValor{
+			Icono = "images/icons/clock.svg",
+			Titulo = "Entrega rápida y confiable",
+			Descripcion = "Entregamos tus productos en tiempo récord dentro de la zona, sin intermediarios ni demoras innecesarias."
+		},new PropuestaValor{
+			Icono = "images/icons/gift.svg",
+			Titulo = "Ofertas pensadas para vos",
+			Descripcion = "Precios justos y promociones semanales en productos de consumo cotidiano, para que siempre ahorres."
+		}, new PropuestaValor{
+			Icono = "images/icons/package.svg",
+			Titulo = "Productos frescos y seleccionados",
+			Descripcion = "Cada artículo es elegido por nuestro equipo garantizando calidad y frescura, como si compraras vos mismo."
+		}, new PropuestaValor{
+			Icono = "images/icons/refresh-cw.svg",
+			Titulo = "Atención cercana y resolutiva",
+			Descripcion = "Ante cualquier inconveniente, resolvemos tu pedido con rapidez y empatía. Queremos que vuelvas a elegirnos."
+		}
+	];
+}
